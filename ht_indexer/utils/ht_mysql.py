436e0a150a0d3e58879568332d2906b51d374a64
Lianet Sepulveda Torres <lisepul@umich.edu>
2023-08-18 15:42:57 -0400

    query MySql

---
+++ ht_indexer/utils/ht_mysql.py
@@ -0,0 +1,29 @@
+import logging
+
+import mysql.connector
+from mysql.connector import Connect
+
+
+def create_mysql_conn(host: str = None, user: str = None, password: str = None, database: str = None):
+    db_conn = None
+
+    if all([host, user, password, database]):
+        db_conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+    else:
+        logging.error('Please pass the valid host, user, password and database')
+        exit()
+
+    return db_conn
+
+
+def query_mysql(db_conn: Connect = None, query: str = None):
+    cursor = db_conn.cursor()
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+    return {name[0]: value for row in results for name, value in zip(cursor.description, row)}
