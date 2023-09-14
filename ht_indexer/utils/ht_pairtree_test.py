b89dacbf0443d38b5414092aa2e039818328021f
Lianet Sepulveda Torres <lisepul@umich.edu>
2023-09-13 23:07:28 -0400

    update pairtree test

--- ht_indexer/utils/ht_pairtree_test.py
+++ ht_indexer/utils/ht_pairtree_test.py
@@ -6,3 +6,5 @@ def test_get_document_pair_path():
 
     path = pairtree.get_pair_path(doc_name)
     assert path == "/39/01/50/51/33/39/15/39015051333915"
+
+# What is the path of this document? What is the right uc2.ark:/13960/t4mk66f1d
