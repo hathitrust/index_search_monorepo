208b634bc643672e9edeef4f05de422f1abe0762
Lianet Sepulveda Torres <lisepul@umich.edu>
2023-09-22 16:33:01 -0400

    Load logs in a file, remove duplicate code and add the name of the files in the loop

--- ht_indexer/document_retriever_service/full_text_search_retriever_service.py
+++ ht_indexer/document_retriever_service/full_text_search_retriever_service.py
@@ -6,6 +6,12 @@ import logging
 import argparse
 import glob
 
+logging.basicConfig(filename='full_text_search_retriever_service.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
@@ -17,6 +23,8 @@ from ht_indexer_api.ht_indexer_api import HTSolrAPI
 
 from utils.ht_mysql import create_mysql_conn
 from utils.text_processor import create_solr_string
+from document_generator.indexer_config import DOCUMENT_LOCAL_PATH
+from ht_document.ht_document import HtDocument
 
 
 class FullTextSearchRetrieverService(CatalogRetrieverService):
@@ -30,48 +38,48 @@ class FullTextSearchRetrieverService(CatalogRetrieverService):
 
         for results in self.retrieve_documents(query, start, rows):
             for record in results:
+
                 if all_items:
-                    for item_id in record.get("ht_id"):
-
-                        logging.info(f"Processing document {item_id}")
-
-                        obj_id = record.get("id").split(".")[1]
-                        logging.info(f"Processing item {obj_id}")
-
-                        try:
-                            entry = self.document_generator.make_document(item_id, record)
-                            yield entry
-                        except Exception as e:
-                            logging.info(f"Document {item_id} failed {e}")
-                            continue
-
-                        yield entry
+                    # Process all the records of a Catalog
+                    total_items = len(record.get("ht_id"))
                 else:
-                    item_id = record.get("ht_id")[0]
-                    logging.info(f"Processing document {item_id}")
+                    # Process the first item of the Catalog record
+                    total_items = 1
+
+                for i in range(0, total_items):
+
+                    item_id = record.get("ht_id")[i]
+
+                    logger.info(f"Processing document {item_id}")
+
+                    # Instantiate each document
+                    ht_document = HtDocument(document_id=item_id)
+
+                    # obj_id = record.get("id").split(".")[1]
+                    logger.info(f"Processing item {ht_document.document_id}")
 
                     try:
-                        entry = self.document_generator.make_document(item_id, record)
-                        yield entry
+                        entry = self.document_generator.make_full_text_search_document(ht_document, record)
+                        # yield entry
                     except Exception as e:
-                        logging.info(f"Document {item_id} failed {e}")
+                        logger.info(f"Document {item_id} failed {e}")
                         continue
-                    yield entry
-                    continue
+
+                    yield entry, ht_document.file_name, ht_document.namespace
 
     @staticmethod
     def clean_up_folder(document_path, list_ids):
-        logging.info("Cleaning up .xml and .Zip files")
+        logger.info("Cleaning up .xml and .Zip files")
 
         for id_name in list_ids:
             # zip file
             list_documents = glob.glob(f"{document_path}/{id_name}.zip")
             for file in list_documents:
-                logging.info(f"Deleting file {file}")
+                logger.info(f"Deleting file {file}")
                 os.remove(file)
             list_documents = glob.glob(f"{document_path}/{id_name}.mets.xml")
             for file in list_documents:
-                logging.info(f"Deleting file {file}")
+                logger.info(f"Deleting file {file}")
                 os.remove(file)
 
 
@@ -124,30 +132,32 @@ def main():
 
     # Create the directory to load the xml files if it does not exit
     try:
-        os.makedirs(os.path.join("/tmp", document_local_path))
+        os.makedirs(os.path.join(DOCUMENT_LOCAL_PATH, document_local_path))
     except FileExistsError:
         pass
 
     count = 0
     query = "*:*"
+    # query = "id:004483341"
     start = 0
     rows = 50
 
-    for entry in document_indexer_service.generate_full_text_entry(query, start, rows, all_items=args.all_items):
+    for entry, file_name, namespace in document_indexer_service.generate_full_text_entry(query, start, rows,
+                                                                                         all_items=args.all_items):
         count = count + 1
         solr_str = create_solr_string(entry)
-        obj_id = entry.get("id").split(".")[1]
 
         with open(
-                f"/{os.path.join('/tmp', document_local_path)}/{obj_id}_solr_full_text.xml",
+                f"/{os.path.join(DOCUMENT_LOCAL_PATH, document_local_path)}/{namespace}{file_name}_solr_full_text.xml",
                 "w",
         ) as f:
             f.write(solr_str)
 
+        print(count)
         # Clean up
-        FullTextSearchRetrieverService.clean_up_folder("/tmp", [obj_id])
-        if count > 150:
-            break
+        FullTextSearchRetrieverService.clean_up_folder(DOCUMENT_LOCAL_PATH, [file_name])
+        # if count > 150:
+        #    break
 
 
 if __name__ == "__main__":
