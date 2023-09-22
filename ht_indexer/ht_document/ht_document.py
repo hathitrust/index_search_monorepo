81645202c42d0b6a831baa0acc9f95f8607d72f5
Lianet Sepulveda Torres <lisepul@umich.edu>
2023-09-22 16:26:50 -0400

    add a class to represent documents and to manage the IO opperations for each document

---
+++ ht_indexer/ht_document/ht_document.py
@@ -0,0 +1,82 @@
+import logging
+from typing import Text
+from utils.ht_pairtree import download_document_file
+from document_generator.indexer_config import (
+    DOCUMENT_LOCAL_PATH,
+    SDR_DIR,
+    TRANSLATE_TABLE,
+)
+
+from pypairtree import pairtree
+
+
+class HtDocument:
+    """
+    This class manage the basic information we need to process a document
+    * ht_id
+    * make sure the document have .zip and mets file
+    * Download the fields and load their path
+    * Rename the files of the document if the id has the following pattern: uc2.ark:/13960/t4mk66f1d
+    """
+
+    def __init__(self, document_id: Text = None):
+        self.document_id = document_id
+
+        self.namespace = HtDocument.get_namespace(document_id)
+        self.obj_id = HtDocument.get_object_id(document_id)
+
+        # If there is special characters in the document_id, the file will be save in the local
+        # environment with a different name
+        self.file_name = pairtree.sanitizeString(self.obj_id)  # self.set_file_name()
+
+        self.source_path = self.get_document_source_path()
+
+        self.target_path = f"{DOCUMENT_LOCAL_PATH}{self.file_name}"
+
+        # Download document .zip and .mets.xml file
+        # TODO: Check if file exist
+        download_document_file(
+            source_path=self.source_path, target_path=self.target_path, extension="zip"
+        )
+
+        # Download document .zip and .mets.xml file
+        # TODO: Check if file exist
+        download_document_file(
+            source_path=self.source_path, target_path=self.target_path, extension="mets.xml"
+        )
+
+    @staticmethod
+    def get_namespace(document_id):
+
+        try:
+            namespace = document_id.split('.')[0]
+            return namespace
+        except ValueError as e:
+            logging.error(f"Review the document id {document_id} {e}")
+
+    @staticmethod
+    def get_object_id(document_id):
+        try:
+            obj_id = document_id.split('.')[1:]
+            return obj_id
+        except ValueError as e:
+            logging.error(f"Review the document id {document_id} {e}")
+
+    def get_document_source_path(self):
+        """
+        If the ht_id contains special characters e.g. uc2.ark:/13960/t4mk66f1d, python can not use this
+        name to read and write the zip file, then this method uses pairtree python package to replace
+        special characters
+        :return:
+        """
+
+        # Use the file name to get the pairtree path
+        doc_path = pairtree.get_pair_path(self.file_name)
+
+        # Escape special characters from the file of the path and the object name
+        doc_translated_path = doc_path.translate(TRANSLATE_TABLE)
+        sanitized_obj_id_translated = self.file_name.translate(TRANSLATE_TABLE)
+
+        source_path = f"{SDR_DIR}/obj/{self.namespace}/pairtree_root{doc_translated_path}/{sanitized_obj_id_translated}"
+
+        return source_path
--- ht_indexer/ht_document/ht_document_test.py
+++ ht_indexer/ht_document/ht_document_test.py
@@ -1,4 +1,5 @@
 from pypairtree import pairtree
+from ht_document.ht_document import HtDocument
 
 
 def test_get_document_pair_path():
@@ -8,19 +9,33 @@ def test_get_document_pair_path():
     assert path == "/39/01/50/51/33/39/15/39015051333915"
 
 
-def test_get_document_pait_path():
+def test_get_document_pairtree_path():
     namespace, obj = "uc2.ark:/13960/t4mk66f1d".split('.')
 
     sanitized_str = pairtree.sanitizeString(obj)
     path = pairtree.get_pair_path(sanitized_str)
     assert path == "/ar/k+/=1/39/60/=t/4m/k6/6f/1d/ark+=13960=t4mk66f1d"
 
-# What is the path of this document? What is the right uc2.ark:/13960/t4mk66f1d
-# scp: /sdr1/obj/uc2/pairtree_root/ar/k://1/39/60//t/4m/k6/6f/1d/ark:/13960/t4mk66f1d/ark:/13960/t4mk66f1d.zip: No such file or directory
-# INFO:root:Download /sdr1/obj/uc2/pairtree_root/ar/k://1/39/60//t/4m/k6/6f/1d/ark:/13960/t4mk66f1d/ark:/13960/t4mk66f1d.zip to /tmp/
-# INFO:root:Document uc2.ark:/13960/t4mk66f1d failed 'int' object is not callable
+
+def test_get_namespace():
+    namespace = HtDocument.get_namespace("uc2.ark:/13960/t4mk66f1d")
+    assert namespace == "uc2"
 
 
-# scp: /sdr1/obj/uc2/pairtree_root/ar/k+/=1/39/60/=f/k6/k0/6x/f8/j/ark+=13960=fk6k06xf8j/ark:/13960/fk6k06xf8j.zip: No such file or directory
-# INFO:root:Download /sdr1/obj/uc2/pairtree_root/ar/k+/=1/39/60/=f/k6/k0/6x/f8/j/ark+=13960=fk6k06xf8j/ark:/13960/fk6k06xf8j.zip to /tmp/
-# /tmp/ark:/13960/fk6k06xf8j.zip
+def test_get_object_id():
+    namespace = HtDocument.get_object_id("uc2.ark:/13960/t4mk66f1d")
+    assert namespace == "ark:/13960/t4mk66f1d"
+
+
+def test_document_several_points():
+    document_id = "miun.adh1541.0001.001"
+
+    namespace = HtDocument.get_namespace(document_id)
+    assert namespace == "miun"
+
+    obj_id = HtDocument.get_object_id(document_id)
+    assert obj_id == "adh1541.0001.001"
+
+
+def test_pairpath_document_several_points():
+    assert "miun,adh1541,0001,001" == pairtree.sanitizeString("miun.adh1541.0001.001")
