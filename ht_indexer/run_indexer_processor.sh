f1a254d775f0df7bb5b4eb05678b9ded32df37f7
Lianet Sepulveda Torres <lisepul@umich.edu>
2023-12-06 16:27:32 -0500

    apllied some change after testing the workflow

--- ht_indexer/run_indexer_processor.sh
+++ ht_indexer/run_indexer_processor.sh
@@ -1,13 +1,14 @@
-
+#!/usr/bin/env bash
 #echo "Build images"
-#docker build -t document_generator .
 
-echo "Setting up document_indexer"
+
+echo "💎 Setting up document indexer service ..."
 
 #docker compose build document_retriever
 docker compose build document_indexer
+docker build -t document_generator .
 
-echo "Starting document indexer service"
-docker compose up document_indexer
+echo "🔥 Starting document indexer service ..."
+docker compose up document_indexer -d
 
 docker compose exec document_indexer python document_indexer_service/document_indexer_service.py --solr_indexing_api http://solr-lss-dev:8983/solr/#/core-x/
--- ht_indexer/run_retriever_processor.sh
+++ ht_indexer/run_retriever_processor.sh
@@ -1,18 +1,16 @@
-
-echo "Build images"
-docker build -t document_generator .
-
+#!/usr/bin/env bash
 echo "💎 Setting up document retriever service ..."
+docker build -t document_generator .
 docker compose build document_retriever
 
 echo "🔥 Starting document retriever service ..."
-docker compose up document_retriever
+docker compose up document_retriever -d
 
 for line in $(cat "$PWD/ht_utils/sample_data/sample_data_ht_ids.txt")
 do
 	ht_id="$line"
 
-	echo "Processing $ht_id record"
+	echo "🔁 Processing record $ht_id"
 
 	docker compose exec document_retriever python document_retriever_service/full_text_search_retriever_service.py --query ht_id:"$ht_id"
 done
