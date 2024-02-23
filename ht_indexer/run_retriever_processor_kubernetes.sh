7f4d429fb4ddf0b0f9a7dc08060f641a9bb19e6b
Lianet Sepulveda Torres <lisepul@umich.edu>
2024-02-22 20:27:58 -0500

    update the script for generating batch of documents in Kubernetes

--- ht_indexer/run_retriever_processor_kubernetes.sh
+++ ht_indexer/run_retriever_processor_kubernetes.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
-# TODO ADD ALL_ITEMS parameter
-#if [ $source_resource=='local' ]; then
-  # You should use the sample_data_creator.sh script to generate the local folder with the documents you want to process
-#for line in $(cat "$PWD/filter_ids.txt")
-#  do
-#    ht_id="$line"
-#    echo "🔁 Processing record $ht_id"
-python document_retriever_service/full_text_search_retriever_service.py --list_ids_path "$PWD/filter_ids.txt" --document_repository pairtree
-    #  docker compose exec document_retriever python document_retriever_service/full_text_search_retriever_service.py --query ht_id:"$ht_id"
-#  done
-#else # This option should work in Kubernetes, not use it locally if you do not have a local folder like a pairtree repository
-#    echo "🔁 Processing all the record included in Catalog image"
-#    docker compose exec document_retriever python document_retriever_service/full_text_search_retriever_service.py --query "*:*" --document_repository pairtree
-#fi
+
+# This script is used to run the retriever processor in a kubernetes environment and having a TXT file with the list of IDs to be processed.
+python document_retriever_service/full_text_search_retriever_by_file.py --list_ids_path "$PWD/filter_ids.txt" --document_repository pairtree
 
 echo "🎉 Done!"
