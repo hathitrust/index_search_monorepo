cd0b75a065a7fad7a9b4de27936287e726c9086e
liseli <lisepul@umich.edu>
2026-09-09 13:01:44 -0500

    get_non_processed_ids() treat a missing file as no IDs processed yet

--- app/ht_indexer/src/document_retriever_service/ht_status_retriever_service.py
+++ app/ht_indexer/src/document_retriever_service/ht_status_retriever_service.py
@@ -8,8 +8,13 @@ logger = get_ht_logger(name=__name__)
 def get_non_processed_ids(
     status_file: str | Path, list_ids: list[str]
 ) -> tuple[list[str], list[str]]:
-    with open(status_file) as f:
-        processed_ids = f.read().splitlines()
-        logger.info(f"Total of items have been processed before {len(processed_ids)}")
-        items2process = list(set(list_ids) - set(processed_ids))
+    try:
+        with open(status_file) as f:
+            processed_ids = f.read().splitlines()
+    except FileNotFoundError:
+        logger.info(f"Status file {status_file} not found. Assuming no IDs processed yet.")
+        processed_ids = []
+
+    logger.info(f"Total of items have been processed before {len(processed_ids)}")
+    items2process = list(set(list_ids) - set(processed_ids))
     return items2process, processed_ids
